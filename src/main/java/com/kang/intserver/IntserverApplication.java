f02d0f7ccf15b4ae9328db082392616a80aea308
wangkang <jswd_50x@163.com>
2019-08-20 16:30:26 +0800

    web

---
+++ src/main/java/com/kang/intserver/IntserverApplication.java
@@ -0,0 +1,15 @@
+package com.kang.intserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class IntserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntserverApplication.class, args);
+    }
+
+}
